0fb091c669386a42bde809bf7cf8de504d9f2444
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-13 06:39:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLeads = ["www.awesome.com", "www.awesome.com", "www.awesome.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+}
