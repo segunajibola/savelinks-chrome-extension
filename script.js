56352235fa597b3310648b53367871df28b9d648
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-19 00:24:49 +0100

    add li items

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
@@ -9,6 +10,14 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tab[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItem = ""
@@ -32,7 +41,7 @@ function render(leads) {
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    render(myleads)
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
