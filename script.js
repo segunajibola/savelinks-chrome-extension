9283a7f4b196eacf3dd34cfdf3ff150685daf316
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-19 00:47:54 +0100

    add li items

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (leadsFromLocalStorage) {
 
 tabBtn.addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tab[0].url)
+        myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     })
